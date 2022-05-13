c6d76b9d70ba4df74f68e186e38804377db5ad7c
Evans Djangbah <djangbahevans@yahoo.com>
2022-05-13 17:40:03 +0000

    Added notifications and friends' screens

--- App.tsx
+++ App.tsx
@@ -2,13 +2,13 @@ import { StatusBar } from "expo-status-bar"
 import React from "react"
 import { StyleSheet, View } from "react-native"
 import { SafeAreaProvider } from "react-native-safe-area-context"
-import PassionsScreen from "./src/screens/PassionsScreen"
+import NotificationsScreen from "./src/screens/NotificationsScreen"
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <View style={styles.container}>
-        <PassionsScreen />
+        <NotificationsScreen />
         <StatusBar style="dark" />
       </View>
     </SafeAreaProvider>
---
+++ src/screens/FriendsScreen.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+import { Pressable, Text, View } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+import LottieView from "lottie-react-native"
+import Button from "../components/Button"
+
+const FriendsScreen = () => {
+  return (
+    <SafeAreaView style={{ flex: 1, alignItems: "center", alignSelf: "center", width: "100%", padding: 40 }}>
+      <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+        <View style={{ flexGrow: 1 }} />
+        <Pressable>
+          <Text style={{ color: "#e94057", fontSize: 16 }}>Skip</Text>
+        </Pressable>
+      </View>
+      <View style={{ marginTop: 88}}>
+        <LottieView
+          autoPlay
+          style={{
+            width: 240,
+            height: 240,
+            backgroundColor: "#eee",
+          }}
+          // Find more Lottie files at https://lottiefiles.com/featured
+          source={require("../../statics/53610-friends.json")}
+        />
+      </View>
+      <Text style={{ fontSize: 24, fontWeight: "bold", marginTop: 64 }}>Search Friends</Text>
+      <Text style={{ textAlign: "center", fontSize: 14, marginTop: 10 }}>You can find your friends from your contact lists to get connected</Text>
+      <Button
+        style={{ marginTop: 120 }}
+        buttonStyle={{ backgroundColor: "#e94057" }}
+        textStyle={{ color: "white" }}
+        title="Access to contact list"
+      />
+    </SafeAreaView>
+  )
+}
+
+export default FriendsScreen
